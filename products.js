de0db9d32ea528d4aef90e9b414eec97c217eea0
sansfrasiscokamal <syaifulkamaludin414@gmail.com>
2026-02-02 21:06:42 +0700

    Push pertama kali

---
+++ products.js
@@ -0,0 +1,34 @@
+const allProducts = [
+    { 
+        id: 1, 
+        name: "Setelan Anak Kaos Salur Stripe & Celana Cargo Usia 1-5 Tahun - Setelan Lengkap Nyaman untuk Anak-Anak", 
+        images: ["img/produk1.jpg"],
+        description: "Bahan : cotton \n motif baju :salur \n jahitan :bagus \n \n silahkan order ",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS91p96gtbpBf-KXLAj/",
+            shopee: "https://s.shopee.co.id/2qOnigUYji",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Setelan Anak Kaos Salur Stripe & Celana Cargo Usia 1-5 Tahun - Setelan Lengkap Nyaman untuk Anak-Anak"
+        }
+    },
+    { 
+        id: 2, 
+        name: "100K 3 Pcs |Kaos Polo Anak Bahan Knit Premium T Shirt Kerah Wangki Laki-laki Perempuan Unisex Cewek Cowok -ALASKA",
+        images: ["img/produk2.jpg"],
+        description: "Bahan : knit premium \n motif : kerah wangki",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS91pCLjVan1e-pYML6/",
+            shopee: "https://s.shopee.co.id/6KyftvEhmu",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Kaos Polo Anak Bahan Knit Premium"
+        }
+    }
+];
+
+// DATA SOSMED UNTUK FOOTER (Pastikan link ini benar)
+const sosmedLinks = {
+    instagram: "https://instagram.com/syaiful_kamaludin",
+    youtube: "https://youtube.com/@syaiful_kamaludin",
+    tiktok: "https://tiktok.com/@syaiful_kamaludin",
+    pinterest: "https://pinterest.com/syaiful_kamaludin",
+    facebook: "https://facebook.com/syaiful_kamaludin",
+    whatsapp: "https://wa.me/6285141632570?text=Halo, saya mau tanya-tanya"
+};
