c7f65d545a2406198ae661a1f6149ac8a9d2620e
sansfrasiscokamal <syaifulkamaludin414@gmail.com>
2026-02-23 04:13:33 +0700

    penambahan gambar

--- products.js
+++ products.js
@@ -20,6 +20,28 @@ const allProducts = [
             shopee: "https://s.shopee.co.id/6KyftvEhmu",
             wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Kaos Polo Anak Bahan Knit Premium"
         }
+    },
+    { 
+        id: 3, 
+        name: "Setelan Anak Hiro Baju Anak Uniseks",
+        images: ["img/produk3.jpg"],
+        description: "SETELAN ANAK HIRO \n Bahan: Katun Baby Terry \n Sesuaikan BERAT BADAN Anak Dalam Memilih Ukuran Baju Jangan Memilih Berdasarkan Umur, UMUR HANYA PERKIRAAN SAJA \n Tersedia 5 Ukuran \n Size S (2-6 Bulan) Max BB 6 Kg = Lebar Dada 28cm, Panjang Baju 33cm, Lebar Pinggul 32cm, Panjang Celana 25cm \n Size M (Max 1 Tahun) Max BB 9 Kg = Lebar Dada 30cm, Panjang Baju 35cm, Lebar Pinggul 34cm, Panjang Celana 27cm Size L (2 Tahun) Max BB 12 Kg = Lebar Dada 32cm, Panjang Baju 38cm, Lebar Pinggul 36cm, Panjang Celana 29cm \n Size XL (3-4 Tahun) Max BB 15 Kg = Lebar Dada 34cm, Panjang Baju 41cm, Lebar Pinggul 38cm, Panjang Celana 31cm \n Size XXL (5-6 Tahun) Max BB 20 Kg = Lebar Dada 37cm, Panjang Baju 45cm, Lebar Pinggul 40cm, Panjang Celana 33cm",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDuQvT3xjr-aDMGH/",
+            shopee: "https://s.shopee.co.id/807QFrGjI1",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Setelan Anak Hiro Baju Anak Uniseks"
+        }
+    },
+    { 
+        id: 4, 
+        name: "MILMODE SETELAN ANAK KINGSLEY (6 BULAN - 6 TAHUN)",
+        images: ["img/produk4.jpg"],
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
     }
 ];
 
