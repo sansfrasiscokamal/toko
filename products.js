610623063d2d7908f9d488bea181a4dd1e87008e
sansfrasiscokamal <syaifulkamaludin414@gmail.com>
2026-06-22 21:29:24 +0700

    perbaikan1

--- products.js
+++ products.js
@@ -1,8 +1,11 @@
 const allProducts = [
     { 
         id: 1, 
-        name: "Setelan Anak Kaos Salur Stripe & Celana Cargo Usia 1-5 Tahun - Setelan Lengkap Nyaman untuk Anak-Anak", 
-        images: ["img/produk1.jpg"],
+        name: "Kaos wangki Polo anak laki-laki", 
+        images: ["img/atasanPolo1.jpg", "img/atasanPolo2.jpg", "img/atasanPolo3.jpg"],
+        price: 26.135,
+        grosirPrice: 26.000,
+        minOrder: 12,
         description: "Bahan : cotton \n motif baju :salur \n jahitan :bagus \n \n silahkan order ",
         links: {
             tiktok: "https://vt.tokopedia.com/t/ZS91p96gtbpBf-KXLAj/",
@@ -12,8 +15,11 @@ const allProducts = [
     },
     { 
         id: 2, 
-        name: "100K 3 Pcs |Kaos Polo Anak Bahan Knit Premium T Shirt Kerah Wangki Laki-laki Perempuan Unisex Cewek Cowok -ALASKA",
-        images: ["img/produk2.jpg"],
+        name: "AREA ANAK Atasan Kemeja anak laki-laki",
+        images: ["img/atasanSummer1.jpg", "img/atasanSummer2.jpg"],
+        price: 50000,
+        grosirPrice: 50000,
+        minOrder: 12,
         description: "Bahan : knit premium \n motif : kerah wangki",
         links: {
             tiktok: "https://vt.tokopedia.com/t/ZS91pCLjVan1e-pYML6/",
@@ -23,9 +29,12 @@ const allProducts = [
     },
     { 
         id: 3, 
-        name: "Setelan Anak Hiro Baju Anak Uniseks",
-        images: ["img/produk3.jpg"],
-        description: "SETELAN ANAK HIRO \n Bahan: Katun Baby Terry \n Sesuaikan BERAT BADAN Anak Dalam Memilih Ukuran Baju Jangan Memilih Berdasarkan Umur, UMUR HANYA PERKIRAAN SAJA \n Tersedia 5 Ukuran \n Size S (2-6 Bulan) Max BB 6 Kg = Lebar Dada 28cm, Panjang Baju 33cm, Lebar Pinggul 32cm, Panjang Celana 25cm \n Size M (Max 1 Tahun) Max BB 9 Kg = Lebar Dada 30cm, Panjang Baju 35cm, Lebar Pinggul 34cm, Panjang Celana 27cm Size L (2 Tahun) Max BB 12 Kg = Lebar Dada 32cm, Panjang Baju 38cm, Lebar Pinggul 36cm, Panjang Celana 29cm \n Size XL (3-4 Tahun) Max BB 15 Kg = Lebar Dada 34cm, Panjang Baju 41cm, Lebar Pinggul 38cm, Panjang Celana 31cm \n Size XXL (5-6 Tahun) Max BB 20 Kg = Lebar Dada 37cm, Panjang Baju 45cm, Lebar Pinggul 40cm, Panjang Celana 33cm",
+        name: "kaos polo anak bahan knit premium",
+        images: ["img/kaosPoloAnak1.jpg"],
+        price: 43000,
+        grosirPrice: 43000,
+        minOrder: 6,
+        description: "Bahan : knit premium \n unisex",
         links: {
             tiktok: "https://vt.tokopedia.com/t/ZS9eDuQvT3xjr-aDMGH/",
             shopee: "https://s.shopee.co.id/807QFrGjI1",
@@ -34,8 +43,179 @@ const allProducts = [
     },
     { 
         id: 4, 
-        name: "MILMODE SETELAN ANAK KINGSLEY (6 BULAN - 6 TAHUN)",
-        images: ["img/produk4.jpg"],
+        name: "brave kids Broadstripe",
+        images: ["img/bravekidsBroadstripe1.jpg", "img/bravekidsBroadstripe2.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 5, 
+        name: "kakathaya Dalvin CelanaAnak",
+        images: ["img/kakathayaDalvinCelanaAnak1.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 6, 
+        name: "kaos Anak Motif Kucing Kicau Mania",
+        images: ["img/kaosAnakMotifKucingKucauMania1.jpg", "img/kaosAnakMotifKucingKucauMania2.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 7, 
+        name: "littlevin Setelan Anak Biru",
+        images: ["img/littlevinSetelanAnakBiru1.jpg", "img/littlevinSetelanAnakBiru2.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 8, 
+        name: "littlevin Setelan Anak Fullprint",
+        images: ["img/littlevinSetelanAnakFullprint1.jpg", "img/littlevinSetelanAnakFullprint2.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 9, 
+        name: "milmode Setelan Anak Kingsley",
+        images: ["img/milmodeSetelanAnakKingsley1.jpg", "img/milmodeSetelanAnakKingsley2"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 10, 
+        name: "oneset Kaos Anak Unisex",
+        images: ["img/onesetKaosAnakUnisex1.jpg", "img/onesetKaosAnakUnisex2.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 11, 
+        name: "setelan Anak Bahan Jersey Neck",
+        images: ["img/setelanAnakBahanJerseyNeck1.jpg", "img/setelanAnakBahanJerseyNeck2.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 12, 
+        name: "setelan Anak Baju Unisex",
+        images: ["img/setelanAnakBajuUnisex.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 13, 
+        name: "setelan Anak Casual Laki",
+        images: ["img/setelanAnakCasualLaki1.jpg","img/setelanAnakCasualLaki2.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 14, 
+        name: "setelan Anak Motif F",
+        images: ["img/setelanAnakMotifF.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 15, 
+        name: "setelan Baju Salur Cargo",
+        images: ["img/setelanBajuSalurCargo1.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
+        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        links: {
+            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            shopee: "https://s.shopee.co.id/60MLu54FDm",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+        }
+    },
+    { 
+        id: 16, 
+        name: "shafa Kids Dress Bayi",
+        images: ["img/shafaKidsDressBayi1.jpg"],
+        price: 40000,
+        grosirPrice: 35000,
+        minOrder: 12,
         description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
         links: {
             tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
