834c64909517e28bbe4a1b30751e2b7af28f2504
sansfrasiscokamal <syaifulkamaludin414@gmail.com>
2026-06-26 10:12:39 +0700

    perubahan harga

--- products.js
+++ products.js
@@ -1,226 +1,258 @@
 const allProducts = [
     { 
         id: 1, 
-        name: "Kaos wangki Polo anak laki-laki", 
+        name: "Kaos wangki Polo anak laki-laki dan perempuan moose Logo Sablon DTF Premium Bahan L Pique unisex SizeS-3XL", 
         images: ["img/atasanPolo1.jpg", "img/atasanPolo2.jpg", "img/atasanPolo3.jpg"],
-        price: 26.135,
-        grosirPrice: 26.000,
-        minOrder: 12,
-        description: "Bahan : cotton \n motif baju :salur \n jahitan :bagus \n \n silahkan order ",
+        price: 26235,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"137,7k",
+        terjualshopee:"",
+        description: "Bahan : lacost pique \n usia direkomendasikan : 1-11tahun \n pola: polos \n \n silahkan order ",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS91p96gtbpBf-KXLAj/",
-            shopee: "https://s.shopee.co.id/2qOnigUYji",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Setelan Anak Kaos Salur Stripe & Celana Cargo Usia 1-5 Tahun - Setelan Lengkap Nyaman untuk Anak-Anak"
+            tiktok: "https://vt.tokopedia.com/t/ZS966NsNDn7Be-AA8vw/",
+            shopee: "#",
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Kaos wangki Polo anak laki-laki dan perempuan moose Logo Sablon DTF Premium Bahan L Pique unisex SizeS-3XL"
         }
     },
     { 
         id: 2, 
-        name: "AREA ANAK Atasan Kemeja anak laki-laki",
+        name: "MILMODE- ATASAN ANAK SUMMER (6Bulan-6Tahun)",
         images: ["img/atasanSummer1.jpg", "img/atasanSummer2.jpg"],
-        price: 50000,
-        grosirPrice: 50000,
-        minOrder: 12,
-        description: "Bahan : knit premium \n motif : kerah wangki",
+        price: 25899,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"26,1k",
+        terjualshopee:"",
+        description: "Bahan : Babyterry premium \n motif : bordir",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS91pCLjVan1e-pYML6/",
+            tiktok: "https://vt.tokopedia.com/t/ZS966tAnXWETL-ti0RP/",
             shopee: "https://s.shopee.co.id/6KyftvEhmu",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Kaos Polo Anak Bahan Knit Premium"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE- ATASAN ANAK SUMMER (6Bulan-6Tahun)"
         }
     },
     { 
         id: 3, 
-        name: "kaos polo anak bahan knit premium",
+        name: "kaos polo anak bahan knit premium T-Shirt kerah wangki laki-laki perempuan unisex - Alaska",
         images: ["img/kaosPoloAnak1.jpg"],
-        price: 43000,
-        grosirPrice: 43000,
-        minOrder: 6,
+        price: 43999,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"85k",
+        terjualshopee:"",
         description: "Bahan : knit premium \n unisex",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDuQvT3xjr-aDMGH/",
+            tiktok: "https://vt.tokopedia.com/t/ZS966nYjMFAhg-lKm0l/",
             shopee: "https://s.shopee.co.id/807QFrGjI1",
             wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Setelan Anak Hiro Baju Anak Uniseks"
         }
     },
     { 
         id: 4, 
-        name: "brave kids Broadstripe",
+        name: "BRAVEKIDS - BROADSTRIPE SETELAN ANAK KAOS DAN CELANA COTTON COMBED 24S SABLON PLASTISOL Fashion",
         images: ["img/bravekidsBroadstripe1.jpg", "img/bravekidsBroadstripe2.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
+        price: 59999,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"391,9k",
+        terjualshopee:"",
         description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS966nUhEsuRu-qUZTc/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
             wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
         }
     },
     { 
         id: 5, 
-        name: "kakathaya Dalvin CelanaAnak",
+        name: "KAKATHAYA Dalvin Celana Anak Laki Laki Celana Pendek Anak Laki Laki Celana Harian Anak 1-11 Tahun",
         images: ["img/kakathayaDalvinCelanaAnak1.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 22015,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"428,4k",
+        terjualshopee:"",
+        description: "Celana",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kgX8s3gAD-IAUPx/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk KAKATHAYA Dalvin Celana Anak Laki Laki Celana Pendek Anak Laki Laki Celana Harian Anak 1-11 Tahun"
         }
     },
     { 
         id: 6, 
-        name: "kaos Anak Motif Kucing Kicau Mania",
+        name: "Kaos Anak Motif Kucing Kicau Mania untuk usia 1-12 Tahun",
         images: ["img/kaosAnakMotifKucingKucauMania1.jpg", "img/kaosAnakMotifKucingKucauMania2.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 30999,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"264,3k",
+        terjualshopee:"",
+        description: " ",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kWf4FTr78-lXE8C/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Kaos Anak Motif Kucing Kicau Mania untuk usia 1-12 Tahun"
         }
     },
     { 
         id: 7, 
-        name: "littlevin Setelan Anak Biru",
+        name: "LITTLEVIN SETELAN ANAK PEREMPUAN FULL MOTIF CREAM SERIES KARAKTER 0-5 TAHUN",
         images: ["img/littlevinSetelanAnakBiru1.jpg", "img/littlevinSetelanAnakBiru2.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 22500,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"291,4k",
+        terjualshopee:"",
+        description: "Bahan: katun \n motif : printing",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kpWwL3X57-rgmfH/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk LITTLEVIN SETELAN ANAK PEREMPUAN FULL MOTIF CREAM SERIES KARAKTER 0-5 TAHUN"
         }
     },
     { 
         id: 8, 
-        name: "littlevin Setelan Anak Fullprint",
+        name: "LITTLEVIN - SETELAN ANAK  FULLPRINT  CREAM SERIES KARAKTER 0-5 TAHUN",
         images: ["img/littlevinSetelanAnakFullprint1.jpg", "img/littlevinSetelanAnakFullprint2.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 30000,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"324,5k",
+        terjualshopee:"",
+        description: "Bahan: katun \n motif : printing",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kGFbCRKJX-WKoS0/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk LITTLEVIN - SETELAN ANAK  FULLPRINT  CREAM SERIES KARAKTER 0-5 TAHUN"
         }
     },
     { 
         id: 9, 
-        name: "milmode Setelan Anak Kingsley",
+        name: "MILMODE - SETELAN ANAK KINGSLEY (6 BULAN - 6 TAHUN)",
         images: ["img/milmodeSetelanAnakKingsley1.jpg", "img/milmodeSetelanAnakKingsley2"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
+        price: 30498,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"55,1k",
+        terjualshopee:"",
         description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96ktM3FKasR-2jmNd/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
             wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
         }
     },
     { 
         id: 10, 
-        name: "oneset Kaos Anak Unisex",
+        name: "ONESET Kaos Anak Unisex 1-10 Tahun (Premium Cotton Combed 30S) - Setelan Anak 1 2 3 4 5 6 7 8 9 10 Tahun Little Yukki",
         images: ["img/onesetKaosAnakUnisex1.jpg", "img/onesetKaosAnakUnisex2.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 68000,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"20,5k",
+        terjualshopee:"",
+        description: "Bahan: katun \n jenis pakaian : setelan",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kndcuXweD-0xW9u/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
             wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
         }
     },
     { 
         id: 11, 
-        name: "setelan Anak Bahan Jersey Neck",
+        name: "STELAN ANAK SETELAN BAHAN JERSEY V NECK KARAKTER LUFFY ROBLOX DEMON SLAYER UK M-XXL TERBARU Bahan Lembut Halus Adem Nyaman Cocok untuk Anak Printing Fullprint Gambar Awet Jahitan Rapi dan Kuat KICAU MANIA VIRAL",
         images: ["img/setelanAnakBahanJerseyNeck1.jpg", "img/setelanAnakBahanJerseyNeck2.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 42000,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"273,2k",
+        terjualshopee:"",
+        description: "Bahan: jersey \n printing : fullprint",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kpRAeGQXm-BuSuh/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk STELAN ANAK SETELAN BAHAN JERSEY V NECK"
         }
     },
     { 
         id: 12, 
-        name: "setelan Anak Baju Unisex",
+        name: "Setelan Anak Hiro Baju Anak Unisex",
         images: ["img/setelanAnakBajuUnisex.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 33700,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"65,7k",
+        terjualshopee:"",
+        description: "Bahan: katun Babyterry \n jenis pakaian: setelan",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96k74pFx6nV-6Udcb/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk Setelan Anak Hiro Baju Anak Unisex"
         }
     },
     { 
         id: 13, 
-        name: "setelan Anak Casual Laki",
+        name: "SETELAN ANAK CASUAL LAKI LAKI TERBARU USIA 3-9 TAHUN ALA KOREAN STYLE VARIASI HOODIE",
         images: ["img/setelanAnakCasualLaki1.jpg","img/setelanAnakCasualLaki2.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 48290,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"31,7k",
+        terjualshopee:"",
+        description: "Bahan: waffle uniqlo \n Desain : Casual",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kvDxKM1rh-q4GEP/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk SETELAN ANAK CASUAL LAKI LAKI TERBARU USIA 3-9 TAHUN ALA KOREAN STYLE VARIASI HOODIE"
         }
     },
     { 
         id: 14, 
-        name: "setelan Anak Motif F",
+        name: "setelan anak baju dan celana laki laki kids 1-5th/baju anak murah",
         images: ["img/setelanAnakMotifF.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 28800,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"61,9k",
+        terjualshopee:"",
+        description: "Bahan: Babyterry \n set anak baju dan celana",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kvGfDMDNb-eo64P/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk setelan anak baju dan celana laki laki kids 1-5th/baju anak murah"
         }
     },
     { 
         id: 15, 
-        name: "setelan Baju Salur Cargo",
+        name: "SETELAN BAJU SALUR DAN CELANA PENDEK CARGO STIK BALIK",
         images: ["img/setelanBajuSalurCargo1.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 34710,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"44,3k",
+        terjualshopee:"",
+        description: "Bahan: Babyterry Premium \n motif : salur",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kcrY86ojH-eSJSH/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk SETELAN BAJU SALUR DAN CELANA PENDEK CARGO STIK BALIK "
         }
     },
     { 
         id: 16, 
-        name: "shafa Kids Dress Bayi",
+        name: "SHAFA KIDS SHOP / DRESS BAYI PEREMPUAN USIA 0 - 18 BULAN BAHAN RAYON VISCOSE / BAJU BAYI PEREMPUAN NEWBORN / FASHION BAYI DAN ANAK PEREMPUAN Kancing",
         images: ["img/shafaKidsDressBayi1.jpg"],
-        price: 40000,
-        grosirPrice: 35000,
-        minOrder: 12,
-        description: "Bahan: Babyterry Premium & Dropneedle Rib Tebal \n motif : bordir",
+        price: 26966,
+        grosirPrice: 0,
+        minOrder: 0,
+        terjualtiktok:"851,6k",
+        terjualshopee:"",
+        description: "Bahan: Rayon viscose \n hiasan: kupu-kupu",
         links: {
-            tiktok: "https://vt.tokopedia.com/t/ZS9eDXA1TasVX-FLNlk/",
+            tiktok: "https://vt.tokopedia.com/t/ZS96kct6E1VUt-joyZD/",
             shopee: "https://s.shopee.co.id/60MLu54FDm",
-            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk MILMODE SETELAN ANAK KINGSLEY"
+            wa: "https://wa.me/6285141632570?text=Asalamualaikum, saya mau tanya produk SHAFA KIDS SHOP / DRESS BAYI PEREMPUAN USIA 0 - 18 BULAN BAHAN RAYON VISCOSE / BAJU BAYI PEREMPUAN NEWBORN / FASHION BAYI DAN ANAK PEREMPUAN Kancing"
         }
     }
 ];
